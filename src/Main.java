cb6e64f892cb155b2241fd1cb60ef56919e6ad12
Kevin-S04 <kevinsimbana2004@outlook.com}>
2025-08-07 15:26:03 -0500

    Conexion

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import model.Conexion;
 import view.LoginForm;
+
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args){
         new LoginForm().setVisible(true);
+        new Conexion();
     }
 }
--- src/model/Conexion.java
+++ src/model/Conexion.java
@@ -1,4 +1,36 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexion {
+
+    //Conexion  a la base de datos
+    /** Credenciales para la conexión a la base de datos. */
+    private final String usuario= "usyijmttc8yx7jxp";
+    private final String url= "jdbc:mysql://usyijmttc8yx7jxp:iIJqMfJ13pTdsgOHsnlQ@b6pl6zmmlwf8z3okn24x-mysql.services.clever-cloud.com:3306/b6pl6zmmlwf8z3okn24x";
+    private final String password="iIJqMfJ13pTdsgOHsnlQ";
+
+    /**
+     * Constructor de la clase. Intenta establecer una conexión para verificar su éxito.
+     */
+    public Conexion(){
+        try{
+            Connection conn = DriverManager.getConnection(url,usuario,password);
+            System.out.println("Conexion exitosa a Clever Cloud");
+            conn.close();
+        }catch (SQLException e){
+            System.out.println("Error al conectar: "+ e.getMessage());
+        }
+    }
+
+    /**
+     * Obtiene y retorna una nueva conexión a la base de datos.
+     * @return Un objeto Connection que representa la conexión a la base de datos.
+     * @throws SQLException Si ocurre un error de conexión.
+     */
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, usuario, password);
+    }
 }
